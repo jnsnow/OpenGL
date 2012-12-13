9472b717c5b1faae8c9689f46bffb23269141063
C.Compton <christopher.j.compton@gmail.com>
2012-12-13 17:32:00 -0500

    Updated shaders to test ifTextured.

--- Flythrough/shaders/vterrain.glsl
+++ Flythrough/shaders/vterrain.glsl
@@ -8,17 +8,11 @@ varying vec4 color;
 varying vec4 fPosition;
 varying vec2 outtexture;
 
-// The below samplers are not needed in this shader.. tested and confirmed.
-//uniform sampler2D gSampler0;
-//uniform sampler2D gSampler1;
-//uniform sampler2D gSampler2;
-
 // position/movement
 uniform mat4 P;
 uniform mat4 R;
 uniform mat4 T;
 uniform mat4 CTM; // CTM is either PRT or TRP, depending.
-
 
 void main() {
 
@@ -26,5 +20,4 @@ void main() {
      color = vColor;
      outtexture = vTex;
      fPosition = vPosition;
-
 }
